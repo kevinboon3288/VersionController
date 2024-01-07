fdcb88744ffe82cfc394b90bc561baa39cb5d1ec
Tan Chee Boon <kevintan7227@gmail.com>
2024-01-07 18:09:53 +0800

    add icons to Resource project

--- ViewModels/VersionControllerViewModel.cs
+++ ViewModels/VersionControllerViewModel.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Prism.Mvvm;
 
 namespace VersionController.ViewModels
 {
-    public class VersionControllerViewModel
+    public class VersionControllerViewModel : BindableBase
     {
     }
 }
