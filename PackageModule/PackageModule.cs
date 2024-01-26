bde0f08732f216673a5d864d5718dfb9acffc642
Tan Chee Boon <tan.cb@aem.com.sg>
2024-01-26 11:08:16 +0800

    Add DotNugetPackgeView and ViewModel

--- PackageModule/PackageModule.cs
+++ PackageModule/PackageModule.cs
@@ -22,17 +22,23 @@ namespace VersionController.PackageModule
 
         public void OnInitialized(IContainerProvider containerProvider)
         {
+            _regionManager.RegisterViewWithRegion("PackageContentRegion", typeof(PackageView));
+            _regionManager.RegisterViewWithRegion("DotNugetPackageContentRegion", typeof(DotNugetPackageListView));
             _regionManager.RegisterViewWithRegion("PackageListContentRegion", typeof(PackageListView));
             
             //TODO: Update this implementation if there is a better way for first navigation
-            IRegion region = _regionManager.Regions["PackageListContentRegion"];
-            region.RequestNavigate(nameof(PackageListView));
+            IRegion region = _regionManager.Regions["PackageContentRegion"];
+            region.RequestNavigate(nameof(PackageView));
         }
 
         public void RegisterTypes(IContainerRegistry containerRegistry)
         {
+            containerRegistry.RegisterForNavigation<PackageView>();
+            containerRegistry.RegisterForNavigation<DotNugetPackageListView>();
             containerRegistry.RegisterForNavigation<PackageListView>();
            
+            ViewModelLocationProvider.Register<PackageView, PackageViewModel>();
+            ViewModelLocationProvider.Register<DotNugetPackageListView, DotNugetPackageListViewModel>();
             ViewModelLocationProvider.Register<PackageListView, PackageListViewModel>();
         }
     }
